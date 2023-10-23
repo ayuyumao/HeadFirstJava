20c961786999b16aaed9c5d28cc6342f2ac85fa0
阿鱼与猫 <ayuyumao@163.com>
2023-10-24 00:19:24 +0800

    提交chapter1
    源代码

---
+++ chapter1/Party.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.Panel;
+import java.awt.event.*;
+
+class Party{
+    public void buildnvite(){
+        Frame f = new Frame();
+        Label l = new Label("Party at Tim's");
+        Button b = new Button("You bet");
+        Button c = new Button("Shoot me");
+        Panel p = new Panel();
+        p.add(l);
+    }
+}
